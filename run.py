98b9fb1eabaa4ffd534126ee2393bd1cfab8e937
Alireza1044 <alireza_1044@yahoo.com>
2024-03-18 23:32:52 -0700

    Add pipeline template

--- run.py
+++ run.py
@@ -1,10 +1,47 @@
-if data == "mnist:
-  # process
-  # train
-  # evaluate
+import time
+
+def process_mnist():
   pass
+
+def train_mnist():
+  pass
+
+def evaluate_mnist():
+  pass
+
+def process_boston():
+  pass
+
+def train_boston():
+  pass
+
+def evaluate_boston():
+  pass
+
+
+if data == "mnist":
+  s = time.time()
+  process_mnist()
+  run_time_process = int(time.time() - s)
+
+  s = time.time()
+  train_mnist()
+  run_time_train = int(time.time() - s)
+  
+  s = time.time()
+  evaluate_mnist()
+  run_time_process = int(time.time() - s)
+
 elif data == "boston":
-  # process
-  # train
-  # evaluate
-  pass
+  s = time.time()
+  process_boston()
+  run_time_process = int(time.time() - s)
+
+  s = time.time()
+  train_boston()
+  run_time_train = int(time.time() - s)
+
+  s = time.time()
+  evaluate_boston()
+  run_time_process = int(time.time() - s)
+
