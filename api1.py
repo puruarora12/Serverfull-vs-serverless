cdb30a653beae08c5d5faafb4dc4fbc55c5334f2
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-04-07 04:57:24 -0700

    Updated code, requirement.txt, YOLO weights

--- api1.py
+++ api1.py
@@ -1,7 +1,9 @@
 import os
 import io
 import boto3
+import awsgi
 import numpy as np
+import json
 from PIL import Image
 from flask import Flask, request, jsonify
 from ultralytics import YOLO # Assuming you have a YOLO model file named 'yolo_model.py'
@@ -9,8 +11,8 @@ from ultralytics import YOLO # Assuming you have a YOLO model file named 'yolo_m
 app = Flask(__name__)
 
 s3 = boto3.client('s3')
-input_bucket = 'data'
-output_bucket = 'predictions'
+input_bucket = 'ds756data'
+output_bucket = 'ds756predictions'
 
 def save_image_to_s3(image, bucket, key):
     # Save the image to the specified S3 bucket
@@ -51,15 +53,15 @@ def load_image_from_s3(bucket, key):
     return image
 
 @app.route('/detect', methods=['POST'])
-def detect():
-    file = request.files['image']
-    filename = file.filename
+def detect(bucket, key):
+    # file = request.files['image']
+    # filename = file.filename
 
     # Save the image to the input S3 bucket
-    save_image_to_s3(file, input_bucket, filename)
+    # save_image_to_s3(file, input_bucket, filename)
 
     # Load the image from the input S3 bucket
-    image = load_image_from_s3(input_bucket, filename)
+    image = load_image_from_s3(input_bucket, key)
 
     # Perform object detection using the YOLO model
     local_weight_file = 'best.pt'
@@ -71,7 +73,7 @@ def detect():
     annotated_image = yolo_model.draw_boxes(results, image)
 
     # Save the annotated image to the output S3 bucket
-    output_key = f'output/{filename}'
+    output_key = f'output/{key}'
     save_image_to_s3(annotated_image, output_bucket, output_key)
 
     # Generate a public URL for the annotated image
@@ -101,3 +103,22 @@ def download_weights_from_s3(bucket, key, local_filename):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
+
+def lambda_handler(event , context):
+    if event['Records']:
+        s3_event = event['Records'][0]['s3']
+        bucket_name = s3_event['bucket']['name']
+        object_key = s3_event['object']['key']
+
+        # Call your Flask app's detect function
+        result = app.detect(bucket_name, object_key)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(result)
+        }
+    else:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Invalid event format')
+        }
